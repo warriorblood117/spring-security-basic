f2c042eaff952a6dfa7fcb65f8fa523f11f155f9
warriorblood117 <warriorblood117@gmail.com>
2024-05-18 22:57:42 -0500

    add login endpoint

--- src/main/java/com/app/controllers/TestAuthController.java
+++ src/main/java/com/app/controllers/TestAuthController.java
@@ -2,50 +2,76 @@ package com.app.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
-@PreAuthorize("denyAll()")
 @CrossOrigin("http://localhost:4200")
 public class TestAuthController {
 
-    @GetMapping("/get")
-@PreAuthorize("hasAuthority('CREATE')")
-public ResponseEntity<Map<String, String>> helloGet() {
-    Map<String, String> response = new HashMap<>();
-    response.put("message", "Hello World - GET");
-    return ResponseEntity.ok(response);
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+        System.out.println(username + password);
+        
+
+        // Crear un objeto de autenticación con el nombre de usuario y la contraseña recibidos
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(username, password));
+
+        // Establecer el objeto de autenticación en el contexto de seguridad
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        Map<String,String> response = new HashMap<>();
+        response.put("message", "Autenticación exitosa");
+        // Si se llega a este punto, la autenticación ha sido exitosa
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PreAuthorize("hasAuthority('CREATE')")
+    @PostMapping("/create")
+    public ResponseEntity<String> createResource() {
+        return ResponseEntity.ok("Recurso creado");
+    }
+
+    @PreAuthorize("hasAuthority('READ')")
+    @PostMapping("/read")
+    public ResponseEntity<String> readResource() {
+        return ResponseEntity.ok("Recurso leído");
+    }
+
+    @PreAuthorize("hasAuthority('UPDATE')")
+    @PostMapping("/update")
+    public ResponseEntity<String> updateResource() {
+        return ResponseEntity.ok("Recurso actualizado");
+    }
+
+    @PreAuthorize("hasAuthority('DELETE')")
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteResource() {
+        return ResponseEntity.ok("Recurso eliminado");
+    }
+
+    @PreAuthorize("hasAuthority('REFACTOR')")
+    @PostMapping("/refactor")
+    public ResponseEntity<String> refactorResource() {
+        return ResponseEntity.ok("Recurso refactorizado");
+    }
 }
 
-    @PostMapping("/post")
-    public String helloPost() {
-        return "Hello World - POST";
-    }
-
-    @PutMapping("/put")
-    public String helloPut() {
-        return "Hello World - PUT";
-    }
-
-    @DeleteMapping("/delete")
-    public String helloDelete() {
-        return "Hello World - DELETE";
-    }
-
-    @PatchMapping("/patch")
-    public String helloPath() {
-        return "Hello World - PATCH";
-    }
-
-}
