4dfb40e5aeb0c34c9abcbbc8676f43076f3f5ceb
warriorblood117 <warriorblood117@gmail.com>
2024-05-19 09:50:58 -0500

    add: jwt dependency and jwtutil for createToken

---
+++ src/main/java/com/app/util/JwtUtils.java
@@ -0,0 +1,53 @@
+package com.app.util;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtUtils {
+
+    @Value("${security.jwt.key.private}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    private String createToken(Authentication authentication) {
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        String username = authentication.getPrincipal().toString();
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(",")); // READ,WRITE , ETC
+
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+
+        return jwtToken;
+
+    }
+
+}
